c6fecc8fc662bfa35f1f0c35d48958fa96ade48f
Dilpreet-369 <singh.dilpreet.flux@gmail.com>
2026-04-17 20:41:33 +0530

    removed mongoose dep

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
-import connectDB from "./db/db.js";
 import app from "./app.js";
 
+// 1. Config environment variables
 dotenv.config({ path: ".env" });
 
-connectDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.error("Error in app:", error);
-      process.exit(1);
-    });
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 5000}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to app:", error);
+const PORT = process.env.PORT || 5000;
+
+try {
+  app.on("error", (error) => {
+    console.error("Error in app:", error);
     process.exit(1);
   });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Failed to start the server:", error);
+  process.exit(1);
+}
